b3e7fc9191326a7314a24bd402336fb7dfd584f5
Daniel Jimenez <danieljimenezcanales@gmail.com>
2020-10-23 12:02:37 -0500

    Add Json Response

--- Bodeguin.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Bodeguin.Application
-{
-    public class Class1
-    {
-    }
-}
---
+++ Bodeguin.Application/Communication/JsonResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bodeguin.Application.Communication
+{
+    public class JsonResult<TData>
+    {
+        public bool Valid { get; set; }
+        public string Message { get; set; }
+        public TData Data { get; set; }
+        public JsonResult() { }
+        public JsonResult(bool valid = true, TData data = default, string message = null)
+        {
+            Valid = valid;
+            Data = data;
+            Message = message;
+        }
+    }
+}
